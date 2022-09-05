ddb80368abfff46a35be781b8c7dde5e7bd100b7
Osman Rahimi <tonijaa007@hotmail.com>
2022-09-05 16:32:42 +0200

    update content/main.js , tsconfig.jon

--- content/main.js
+++ content/main.js
@@ -5,7 +5,7 @@ export const attributes = {
       link: '/#portofolio',
       text: 'My work',
     },
-    heading: "I'm Paco Cu, and I create",
+    heading: "I'm Osman, and I create special nuts",
     image: '/img/hero.jpeg',
     secondButton: { icon: 'whatsapp', link: '/#contact', text: 'Contact' },
     social: {
